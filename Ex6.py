625f9a054524e714c675392bc639795372e3c61b
StanleyChacko <43635466+StanleyChacko@users.noreply.github.com>
2018-09-30 17:20:37 +0100

    Create Ex6.py

---
+++ Ex6.py
@@ -0,0 +1,14 @@
+class menu:
+    def add(self,item,price):
+        print item
+        print price
+        
+
+    def show(self):
+        
+        print 'hello'
+        
+m=menu()
+m.add("idly",10)
+m.add("vada",20)
+m.show()
